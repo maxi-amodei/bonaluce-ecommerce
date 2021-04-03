2d0f246a61c9b9c0691232b77235278675cae835
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-03 13:01:59 -0300

    Created products and categories controller, and some seeds from console

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-has_many :product_categories, dependent: :destroy
-has_many :products, through: :product_categoties
+  has_many :product_categories, dependent: :destroy
+  has_many :products, through: :product_categories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
   resources :products
   resources :categories, only: [:show]
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
