ab44b5a7953c66363fcb370ae8719015ca56a4ab
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-14 16:00:54 -0300

    Adding devise controller to configure redirecting after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-    
+  before_action :persist_last_visited_path, :authenticate_user!
+  
+  
+  private
+
+  def persist_last_visited_path
+    ignored_paths = %W(/users/sign_in /users/sign_up /users/password /users/sign_out /users/confirm_password /added_to_cart_items)
+    unless ignored_paths.include?(request.path) || request.xhr?
+     session[:last_visited_path] = request.path
+    end
+   end
 end
