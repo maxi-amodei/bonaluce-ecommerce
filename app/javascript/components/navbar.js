a89cd07849c71fef5f422aad3955a688c3ac443c
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-16 16:56:51 -0300

    added phone icon and solved proble in navbar js

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,16 +1,21 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar');
+  const userIcon = document.querySelector('.fa-user-circle');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= navbar.offsetHeight) {
         navbar.style.padding = "7px 20px";
         document.querySelectorAll('.nav-link').forEach(item => item.style.fontSize = "12px");
-        document.querySelector('.fa-3x').style.fontSize = "2.1em";
+        if(userIcon){
+          userIcon.style.fontSize = "2.1em";
+        }
 
       } else {
         navbar.style.padding = "15px 40px";
         document.querySelectorAll('.nav-link').forEach(item => item.style.fontSize = "14px");
-        document.querySelector('.fa-3x').style.fontSize = "3em";
+        if(userIcon){
+          userIcon.style.fontSize = "3em";
+        }
 
       }
     });
