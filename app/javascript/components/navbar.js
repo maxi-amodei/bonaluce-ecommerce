931db75962cafe583418617c228f48672ec5b93a
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-16 15:15:20 -0300

    adding logo to nav and footer, and style to navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,10 +3,14 @@ const initUpdateNavbarOnScroll = () => {
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= navbar.offsetHeight) {
-        navbar.classList.add('fixed-top');
+        navbar.style.padding = "7px 20px";
+        document.querySelectorAll('.nav-link').forEach(item => item.style.fontSize = "12px");
+        document.querySelector('.fa-3x').style.fontSize = "2.1em";
 
       } else {
-        navbar.classList.remove('fixed-top');
+        navbar.style.padding = "15px 40px";
+        document.querySelectorAll('.nav-link').forEach(item => item.style.fontSize = "14px");
+        document.querySelector('.fa-3x').style.fontSize = "3em";
 
       }
     });
