a49b3b720d6c1d6dafa53c0be696ae542fdbd638
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-22 13:26:36 -0300

    Fixing bug with cloudinary config

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,16 +12,23 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @categories = Category.all
     @colors = colors
   end
 
   def create
-    raise
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
   end
+  
   private
 
   def product_params
-    params.require(:product).permit(:name, :short_description, :supplier, :color, :price, :product_height, :product_length, :product_diameter, photos: [])
+    params.require(:product).permit(:name, :short_description, :supplier_id, :color, :price, :product_height, :product_length,:product_width, :product_diameter,category_ids: [], photos: [])
   end
 
   def colors
